085c0dee05a13621bcf2e10d0ee9e592dd77bd96
ITJacob <jacob.wd8816@gmail.com>
2023-06-16 21:56:19 +0800

    3 files modified

--- select.ts
+++ select.ts
@@ -2,16 +2,33 @@
 export function initSelectValue(values: { [s: string]: string[] }, cb) {
   const select = document.getElementById('item-select');
   const list = document.getElementById('item-select-list');
+  let selected;
 
   Object.keys(values)
     .sort()
     .forEach((key) => {
-      let option = document.createElement('option');
-      option.setAttribute('value', key);
-      list.appendChild(option);
+      let li = document.createElement('li');
+      // li.setAttribute('value', key);
+
+      let optionText = document.createTextNode(key);
+      li.appendChild(optionText);
+
+      list.appendChild(li);
     });
 
-  select.addEventListener('change', (e) => {
-    cb((e.target as HTMLSelectElement).value);
+  select.addEventListener('input', (e: InputEvent) => {
+    // cb((e.target as HTMLSelectElement).value);
+    for (let i of list.children) {
+      const res = i.textContent.includes(e.data);
+      (i as HTMLElement).style.opacity = res ? '1' : '0.6';
+    }
+  });
+  list.addEventListener('click', (e) => {
+    cb((e.target as HTMLElement).textContent);
+    (e.target as HTMLElement).style.backgroundColor = 'burlywood';
+    if (selected) {
+      selected.style.backgroundColor = 'white';
+    }
+    selected = e.target;
   });
 }
