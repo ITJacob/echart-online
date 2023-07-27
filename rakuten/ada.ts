1d50419e183608716689b48cfa2a7220f699e235
Wang Dong <jacob.wd8816@gmail.com>
2023-07-27 21:06:17 +0800

    move files

---
+++ rakuten/ada.ts
@@ -0,0 +1,20 @@
+import { getFiles } from '../tools';
+
+// 广告投入
+export async function getAda() {
+  const files = await getFiles('ada');
+  const result = {};
+  for (let key in files) {
+    // 文件分行
+    // 除去首6尾1行无用数据，
+    // 除去首尾双引号，以逗号隔开
+    result[key.slice(0, -4)] = files[key]
+      .split('\n')
+      .slice(6, -1)
+      .map((l) => {
+        const arr = l.slice(1, -1).split('","');
+        return [arr[3].toUpperCase(), arr[8], arr[16]];
+      });
+  }
+  return result;
+}
---
+++ rakuten/data.ts
@@ -0,0 +1,17 @@
+import { getFiles } from '../tools';
+
+// 销售数据
+export async function getData() {
+  const files = await getFiles('data');
+  const result = {};
+  for (let key in files) {
+    // 文件分行
+    // 除去首尾4行无用数据，
+    // 除去首尾双引号，以逗号隔开
+    result[key.slice(2, -4)] = files[key]
+      .split('\n')
+      .slice(4, -4)
+      .map((l) => l.slice(1, -1).split('","'));
+  }
+  return result;
+}
